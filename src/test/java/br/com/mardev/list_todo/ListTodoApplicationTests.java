e1d62566bc683c780f9c4ed50251440001eedb44
Marcosdsj <mfsilva5585@gmail.com>
2025-06-04 16:17:56 -0300

    init

---
+++ src/test/java/br/com/mardev/list_todo/ListTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mardev.list_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
