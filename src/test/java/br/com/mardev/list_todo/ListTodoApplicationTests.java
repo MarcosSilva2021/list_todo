da6a8ba2eac375b8afc5f493e4f146d788973198
Marcosdsj <mfsilva5585@gmail.com>
2025-06-09 16:25:18 -0300

    init Junit

--- src/test/java/br/com/mardev/list_todo/ListTodoApplicationTests.java
+++ src/test/java/br/com/mardev/list_todo/ListTodoApplicationTests.java
@@ -1,13 +1,25 @@
 package br.com.mardev.list_todo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import br.com.mardev.list_todo.entity.Todo;
 
 @SpringBootTest
 class ListTodoApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void testCreateTodoSuccess() {
+		var todo = new Todo("todo 01", "desc todo 1", false, 1);
+	}
+
+	@Test
+	void testCreateTodoFailure() {
 	}
 
 }
