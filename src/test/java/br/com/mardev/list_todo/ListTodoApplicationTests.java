44f178cc3fa192f8a57e2b0672472791e0171998
Marcosdsj <mfsilva5585@gmail.com>
2025-06-13 16:33:32 -0300

    add dependency test

--- src/test/java/br/com/mardev/list_todo/ListTodoApplicationTests.java
+++ src/test/java/br/com/mardev/list_todo/ListTodoApplicationTests.java
@@ -3,11 +3,12 @@ package br.com.mardev.list_todo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import br.com.mardev.list_todo.entity.Todo;
 
-@SpringBootTest()
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ListTodoApplicationTests {
 
 	@Autowired
@@ -25,13 +26,11 @@ class ListTodoApplicationTests {
 			.expectStatus().isOk()
 			.expectBody()
 			.jsonPath("$").isArray()
-			.jsonPath("$.lenght").isEqualTo(1)
+			.jsonPath("$.length").isEqualTo(1)
 			.jsonPath("$[0].nome").isEqualTo(todo.getNome())
 			.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
 			.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
 			.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
-
-
 
 	}
 
